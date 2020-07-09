4e21fba0ac84f58feff766b5df2f1d20e72e68d4
Henri Buyse <henri.buyse@gmail.com>
2020-07-09 15:48:16 +0200

    polybar: correctlt handle the multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,15 +2,14 @@
 
 # Terminate already running bar instances
 killall -q polybar
+
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
-
-if type "xrandr"; then
+if [[ -n "$(which xrandr)" ]]; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload example &
   done
 else
   polybar --reload example &
 fi
-
-echo "Bars launched..."
+# vim: set ts=2 sw=2 tw=0 et :
