21e2cade82ab52a227a22f54be30edc830477616
Henri Buyse <henri.buyse@gmail.com>
2020-07-15 21:32:45 +0200

    compton: change backend for CG8250.
    Change backend using args since it was fickering.
    
    
    Former-commit-id: 3719ee2249187981c85aa3b98b986e6db3a8b1c1

--- .config/compton/launch.sh
+++ .config/compton/launch.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+DAEMON=compton
+OPTS="-b --config $HOME/.config/compton/compton.conf"
+
 # Terminate already running compton instances
-killall -q compton
+killall -q $DAEMON 
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x compton >/dev/null; do sleep 1; done
+while pgrep -u $UID -x $DAEMON >/dev/null; do sleep 1; done
 
 # Launch compton in background, using default config location ~/.config/compton/compton.conf
-compton -b --config $HOME/.config/compton/compton.conf
+if [[ "$(hostname)" == "CG8250" ]]; then
+	OPTS="$OPTS --backend=xrender"
+fi
+$DAEMON $OPTS
