ced662421b9e8c0d11064e10f8009ebd999ced8e
Henri Buyse <henri.buyse@gmail.com>
2020-07-16 10:30:36 +0200

    polybar: update nordvpn script
    I updated the script in order to not show datas if nordvpn is not
    installed
    
    
    Former-commit-id: 56e17f0e30ee8a2a628784c15722bc7e78d3c32b

--- .config/polybar/vpn-nordvpn-status.sh
+++ .config/polybar/vpn-nordvpn-status.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-STATUS=$(nordvpn status | grep Status | tr -d ' ' | cut -d ':' -f2)
+DAEMON=nordvpn
 
-if [ "$STATUS" = "Connected" ]; then
-    echo "%{F#82E0AA}%{A1:nordvpn d:}$(nordvpn status | grep City | cut -d ':' -f2)%{A}%{F-}"
-else
-    echo "%{F#f00}%{A1:nordvpn c:}no vpn%{A}%{F-}"
+if [ "$(command -v $DAEMON)" = "$DAEMON" ]; then
+	STATUS=$($DAEMON status | grep Status | tr -d ' ' | cut -d ':' -f2)
+
+	if [ "$STATUS" = "Connected" ]; then
+		echo "%{F#82E0AA}%{A1:$DAEMON d:}$($DAEMON status | grep City | cut -d ':' -f2)%{A}%{F-}"
+	else
+		echo "%{F#f00}%{A1:$DAEMON c:}no vpn%{A}%{F-}"
+	fi
 fi
