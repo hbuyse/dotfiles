783f2a05e0de39a903d20fa4359dad57c811818c
Henri Buyse <henri.buyse@gmail.com>
2020-07-21 20:23:13 +0200

    polybar: fix spotify status script
    Former-commit-id: 870bf3c00d13e0ab7472fe169b45aa36cc00393b

--- .config/polybar/spotify_status.py
+++ .config/polybar/spotify_status.py
@@ -1,62 +1,16 @@
 #!/usr/bin/env python
 
 import sys
-import dbus
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '-t',
-    '--trunclen',
-    type=int,
-    metavar='trunclen'
-)
-parser.add_argument(
-    '-f',
-    '--format',
-    type=str,
-    metavar='custom format',
-    dest='custom_format'
-)
-parser.add_argument(
-    '-p',
-    '--playpause',
-    type=str,
-    metavar='play-pause indicator',
-    dest='play_pause'
-)
-parser.add_argument(
-    '--font',
-    type=str,
-    metavar='the index of the font to use for the main label',
-    dest='font'
-)
-parser.add_argument(
-    '--playpause-font',
-    type=str,
-    metavar='the index of the font to use to display the playpause indicator',
-    dest='play_pause_font'
-)
-parser.add_argument(
-    '-q',
-    '--quiet',
-    action='store_true',
-    help="if set, don't show any output when the current song is paused",
-    dest='quiet',
-)
-
-args = parser.parse_args()
-
+import dbus
 
 def fix_string(string):
-    # corrects encoding for the python version used
-    if sys.version_info.major == 3:
-        return string
-    else:
-        return string.encode('utf-8')
-
+    """Corrects encoding for the python version used"""
+    return string if sys.version_info.major == 3 else string.encode('utf-8')
 
 def truncate(name, trunclen):
+    """Truncate a string"""
     if len(name) > trunclen:
         name = name[:trunclen]
         name += '...'
@@ -64,46 +18,106 @@ def truncate(name, trunclen):
             name += ')'
     return name
 
+class Spotify:
+    """Spotity class get its datas from dbus"""
 
+    def __init__(self):
+        self._session_bus = dbus.SessionBus()
+        self._bus = self._session_bus.get_object(
+            'org.mpris.MediaPlayer2.spotify',
+            '/org/mpris/MediaPlayer2'
+            )
+        self._properties = dbus.Interface(self._bus, 'org.freedesktop.DBus.Properties')
 
-# Default parameters
-output = fix_string(u'{play_pause} {artist}: {song}')
-trunclen = 35
-play_pause = fix_string(u'\uF04B,\uF04C') # first character is play, second is paused
+    @property
+    def metadata(self):
+        """Retrieve metadata from the Dbus connection"""
+        return self._properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
 
-label_with_font = '%{{T{font}}}{label}%{{T-}}'
-font = args.font
-play_pause_font = args.play_pause_font
+    @property
+    def status(self):
+        """Retrieve status from the Dbus connection"""
+        return self._properties.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
 
-quiet = args.quiet
+    @property
+    def position(self):
+        """Retrieve position from the Dbus connection"""
+        return self._properties.Get('org.mpris.MediaPlayer2.Player', 'Position')
 
-# parameters can be overwritten by args
-if args.trunclen is not None:
-    trunclen = args.trunclen
-if args.custom_format is not None:
-    output = args.custom_format
-if args.play_pause is not None:
-    play_pause = args.play_pause
-
-try:
-    session_bus = dbus.SessionBus()
-    spotify_bus = session_bus.get_object(
-        'org.mpris.MediaPlayer2.spotify',
-        '/org/mpris/MediaPlayer2'
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-t',
+        '--trunclen',
+        type=int,
+        metavar='trunclen'
+    )
+    parser.add_argument(
+        '-f',
+        '--format',
+        type=str,
+        metavar='custom format',
+        dest='custom_format'
+    )
+    parser.add_argument(
+        '-p',
+        '--playpause',
+        type=str,
+        metavar='play-pause indicator',
+        dest='play_pause'
+    )
+    parser.add_argument(
+        '--font',
+        type=str,
+        metavar='the index of the font to use for the main label',
+        dest='font'
+    )
+    parser.add_argument(
+        '--playpause-font',
+        type=str,
+        metavar='the index of the font to use to display the playpause indicator',
+        dest='play_pause_font'
+    )
+    parser.add_argument(
+        '-q',
+        '--quiet',
+        action='store_true',
+        help="if set, don't show any output when the current song is paused",
+        dest='quiet',
     )
 
-    spotify_properties = dbus.Interface(
-        spotify_bus,
-        'org.freedesktop.DBus.Properties'
-    )
+    args = parser.parse_args()
 
-    metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
-    status = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
+    # Default parameters
+    output = fix_string(u'{play_pause} {artist}: {song}')
+    trunclen = 35
+    play_pause = fix_string(u'\uF04B,\uF04C') # first character is play, second is paused
+
+    label_with_font = '%{{T{font}}}{label}%{{T-}}'
+    font = args.font
+    play_pause_font = args.play_pause_font
+
+    quiet = args.quiet
+
+    # parameters can be overwritten by args
+    if args.trunclen is not None:
+        trunclen = args.trunclen
+    if args.custom_format is not None:
+        output = args.custom_format
+    if args.play_pause is not None:
+        play_pause = args.play_pause
+
+    try:
+        spotify = Spotify()
+
+        metadata = spotify.metadata
+        status = spotify.status
+
+    except Exception as expt:
+        print(expt)
 
     # Handle play/pause label
-
     play_pause = play_pause.split(',')
-
     if status == 'Playing':
         play_pause = play_pause[0]
     elif status == 'Paused':
@@ -129,13 +143,9 @@ try:
             album = label_with_font.format(font=font, label=album)
 
         # Add 4 to trunclen to account for status symbol, spaces, and other padding characters
-        print(truncate(output.format(artist=artist, 
-                                     song=song, 
-                                     play_pause=play_pause, 
+        print(truncate(output.format(artist=artist,
+                                     song=song,
+                                     play_pause=play_pause,
                                      album=album), trunclen + 4))
 
-except Exception as e:
-    if isinstance(e, dbus.exceptions.DBusException):
-        print('')
-    else:
-        print(e)
+main()
