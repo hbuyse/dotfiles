4d31529cd0e07738e3d48c75048312eab419cd72
Henri Buyse <henri.buyse@gmail.com>
2020-08-21 23:23:44 +0200

    rofi: fix power.sh
    Former-commit-id: 577ccd9cb0b92e930a4e8fad678a7fac4efbd5e8

--- .config/rofi/power.sh
+++ .config/rofi/power.sh
@@ -3,38 +3,40 @@
 OPTIONS="\tLock\n\tLogout\n\tShutdown\n\tReboot\n鈴\tSuspend\n\tCaffeinate\n\tUncaffeinate\n"
 
 lock() {
-	xautolock -locknow -locker "$HOME/.config/i3/i3lock-multi -i $HOME/.config/i3/locker.png"
+    xautolock -locknow
+    return 0
 }
 
 if [ "$@" ]
 then
-	case $@ in
-		*Uncaffeinate)
-            xautolock -disable
-			xset +dpms
-           notify-send "Screen suspend" "Enabled"
-			;;
-		*Caffeinate)
+    case $@ in
+        *Uncaffeinate)
             xautolock -enable
-			xset -dpms
+            xset +dpms
+            notify-send "Screen suspend" "Enabled"
+            ;;
+        *Caffeinate)
+            xautolock -disable
+            xset -dpms
             notify-send "Screen suspend" "Disabled"
-			;;
-		*Lock)
-			lock
-			;;
-		*Logout)
-			i3-msg exit
-			;;
-		*Shutdown)
-			lock && systemctl poweroff
-			;;
-		*Reboot)
-			lock && systemctl reboot
-			;;
-		*Suspend)
-			lock && systemctl suspend
-			;;
-	esac
+            ;;
+        *Lock)
+            lock
+            ;;
+        *Logout)
+            i3-msg exit
+            ;;
+        *Shutdown)
+            lock && systemctl poweroff
+            ;;
+        *Reboot)
+            lock && systemctl reboot
+            ;;
+        *Suspend)
+            lock && systemctl suspend
+            ;;
+    esac
 else
-	echo -e $OPTIONS
+    echo -e $OPTIONS
 fi
+# vim: set ts=4 sw=4 tw=0 et :
