a6e1a7ff22a811dc58d849adefc1862ee9e5073f
Henri Buyse <henri.buyse@gmail.com>
2020-08-27 19:14:10 +0200

    pactl: add t480 support
    Former-commit-id: eb794aafa4532cd863456997cd6050cf56b80f92

--- .config/i3/setup_pactl.sh
+++ .config/i3/setup_pactl.sh
@@ -2,7 +2,7 @@
 HOSTNAME="$(hostname -s | tr '[:upper:]' '[:lower:]')"
 
 # WORK
-if [ "$HOSTNAME" = "henrib-latitude-5400" ]; then
+if [ "$HOSTNAME" = "henrib-latitude-5400" ] || [ "$HOSTNAME" = "t480" ]; then
     # Use internal PCI card
     pactl set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo
 
