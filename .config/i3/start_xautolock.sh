46872a905f8521f663c79e2850b13ea46caeef6c
Henri Buyse <henri.buyse@gmail.com>
2020-08-31 21:32:04 +0200

    i3: xautolock - better script
    Former-commit-id: 43c4d67398781b9385db8397777e3c1d59b5da19

--- .config/i3/start_xautolock.sh
+++ .config/i3/start_xautolock.sh
@@ -1,4 +1,10 @@
 #! /usr/bin/env sh
 
-killall xautolock
-xautolock -time 5 -locker "$HOME/.config/i3/i3lock-multi -i $HOME/.config/i3/locker.png" -killtime 15 -killer "systemctl suspend" -detectsleep -notify 30 -notifier "notify-send -u critical -t 29000 'i3lock' 'Will lock in 30 seconds'"
+DAEMON="xautolock"
+
+killall $DAEMON
+$DAEMON -time 5 -locker "$HOME/.config/i3/i3lock-multi -i $HOME/.config/i3/locker.png" \
+        -killtime 15 -killer "systemctl suspend" \
+        -detectsleep \
+        -notify 30 -notifier "notify-send -u critical -t 29000 'i3lock' 'Will lock in 30 seconds'"
+# vim: set ts=4 sw=4 tw=0 et ft=sh :
