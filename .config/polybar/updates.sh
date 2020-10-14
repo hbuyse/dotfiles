24ed4fe8a9d377e2fa8d0b60f2716e70b524b5ad
Henri Buyse <henri.buyse@gmail.com>
2020-10-14 10:11:54 +0200

    polybar: updates change tab to spaces
    Former-commit-id: 959b153693e10a0ff1631117eacb731f632ad8e6

--- .config/polybar/updates.sh
+++ .config/polybar/updates.sh
@@ -3,15 +3,15 @@
 OS_RELEASE="/usr/lib/os-release"
 
 if [[ -f "$OS_RELEASE" ]]; then
-	DISTRIB=$(grep ID_LIKE $OS_RELEASE | cut -d'=' -f2)
-	case "$DISTRIB" in
-		arch)
-			updates=$(checkupdates 2> /dev/null | wc -l )
-			;;
-		debian)
-			updates=$(apt list --upgradable 2> /dev/null | grep -c upgradable);
-			;;
-	esac
+    DISTRIB=$(grep ID_LIKE $OS_RELEASE | cut -d'=' -f2)
+    case "$DISTRIB" in
+        arch)
+            updates=$(checkupdates 2> /dev/null | wc -l )
+            ;;
+        debian)
+            updates=$(apt list --upgradable 2> /dev/null | grep -c upgradable);
+            ;;
+    esac
 fi
 
 if [[ $updates -gt 0 ]]; then
@@ -19,3 +19,4 @@ if [[ $updates -gt 0 ]]; then
 else
     echo ""
 fi
+# vim: set ts=4 sw=4 tw=0 et ft=sh :
