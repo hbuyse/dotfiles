45afd2259a136d3affb4e0fc72c649b401419f23
Henri Buyse <henri.buyse@gmail.com>
2020-11-03 16:05:09 +0100

    i3/xautolock: load from the 'wallpapers' directory
    Former-commit-id: 2ebfe2c709f69eea95312f9c2415c3fda7a47ad8

--- .config/i3/start_xautolock.sh
+++ .config/i3/start_xautolock.sh
@@ -3,7 +3,7 @@
 DAEMON="xautolock"
 
 killall $DAEMON
-$DAEMON -time 5 -locker "$HOME/.config/i3/i3lock-multi -i $HOME/.config/i3/locker.png" \
+$DAEMON -time 5 -locker "$HOME/.config/i3/i3lock-multi -i $HOME/.config/wallpapers/locker.png" \
         -killtime 15 -killer "systemctl suspend" \
         -detectsleep \
         -notify 30 -notifier "notify-send -u critical -t 29000 'i3lock' 'Will lock in 30 seconds'"
