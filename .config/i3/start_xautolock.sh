816647170ad6662206374f54782919b2e4ce7582
Henri Buyse <henri.buyse@gmail.com>
2020-11-10 09:10:50 +0100

    xautolock: specify rules based on hostname
    Former-commit-id: e5c17ff6020634681d428cba95f99e8e10bafd6a

--- .config/i3/start_xautolock.sh
+++ .config/i3/start_xautolock.sh
@@ -1,10 +1,21 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
 DAEMON="xautolock"
 
 killall $DAEMON
-$DAEMON -time 5 -locker "$HOME/.config/i3/i3lock-multi -i $HOME/.config/wallpapers/locker.png" \
+
+if ! command -v $DAEMON; then
+    echo "xautolock not found. Exiting"
+    exit
+fi
+
+if [[ $HOSTNAME == "T480" || $HOSTNAME == "CG8250" ]]; then
+    $DAEMON -time 5 -locker "$HOME/.config/i3/i3lock-multi -i $HOME/.config/wallpapers/locker.png" \
         -killtime 15 -killer "systemctl suspend" \
         -detectsleep \
         -notify 30 -notifier "notify-send -u critical -t 29000 'i3lock' 'Will lock in 30 seconds'"
+else
+    $DAEMON -time 5 -locker "$HOME/.config/i3/i3lock-multi -i $HOME/.config/wallpapers/locker.png" \
+        -notify 30 -notifier "notify-send -u critical -t 29000 'i3lock' 'Will lock in 30 seconds'"
+fi
 # vim: set ts=4 sw=4 tw=0 et ft=sh :
