db6e13d52f63aa9f3d767998ae9cfe80aa8ddce5
Henri Buyse <henri.buyse@gmail.com>
2020-11-10 12:13:56 +0100

    xautolock: use bash array to store arguments
    Former-commit-id: f190f25f1ee7e8193cc0dd6685e8fe8dfa714cc6

--- .config/i3/start_xautolock.sh
+++ .config/i3/start_xautolock.sh
@@ -5,17 +5,18 @@ DAEMON="xautolock"
 killall $DAEMON
 
 if ! command -v $DAEMON; then
-    echo "xautolock not found. Exiting"
+    echo "$DAEMON not found. Exiting"
     exit
 fi
 
+DAEMON_ARGS=("-time 5")
+DAEMON_ARGS+=("-locker \"$HOME/.config/i3/i3lock-multi -i $HOME/.config/wallpapers/locker.png\"")
+DAEMON_ARGS+=("-notify 30 -notifier \"notify-send -u critical -t 29000 'i3lock' 'Will lock in 30 seconds'\"")
+
 if [[ $HOSTNAME == "T480" || $HOSTNAME == "CG8250" ]]; then
-    $DAEMON -time 5 -locker "$HOME/.config/i3/i3lock-multi -i $HOME/.config/wallpapers/locker.png" \
-        -killtime 15 -killer "systemctl suspend" \
-        -detectsleep \
-        -notify 30 -notifier "notify-send -u critical -t 29000 'i3lock' 'Will lock in 30 seconds'"
-else
-    $DAEMON -time 5 -locker "$HOME/.config/i3/i3lock-multi -i $HOME/.config/wallpapers/locker.png" \
-        -notify 30 -notifier "notify-send -u critical -t 29000 'i3lock' 'Will lock in 30 seconds'"
+    DAEMON_ARGS+=("-killtime 15 -killer \"systemctl suspend\"")
+    DAEMON_ARGS+=("-detectsleep")
 fi
+
+eval "${DAEMON}" "${DAEMON_ARGS[*]}"
 # vim: set ts=4 sw=4 tw=0 et ft=sh :
