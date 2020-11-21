e0bf833439d6c59eaab035d671693701dd3bddc5
Henri Buyse <henri.buyse@gmail.com>
2020-11-21 18:15:59 +0100

    conky: change conky.date.time to hostname specific ones
    Former-commit-id: 41ea6e089186ba2df4710af187f47bad32d9eb11

--- .config/conky/launch.sh
+++ .config/conky/launch.sh
@@ -2,14 +2,14 @@
 
 DAEMON="conky"
 CONKY_CONFIG_FOLDER="$HOME/.config/conky"
-CONKIES=("$CONKY_CONFIG_FOLDER/conkyrc.date.time" "$CONKY_CONFIG_FOLDER/conkyrc.proc.mem")
+CONKIES=("$CONKY_CONFIG_FOLDER/conkyrc.date.time.$(hostname | tr '[:upper:]' '[:lower:]')" "$CONKY_CONFIG_FOLDER/conkyrc.proc.mem")
 
 # Kill all processes even if the daemon binary does not exist
 # The daemon may have been uninstalled
 killall $DAEMON
 
 # Check that the daemon command exist
-if ! command -v $DAEMON; then
+if ! command -v $DAEMON 2> /dev/null; then
 	echo "Conky not found. Exiting"
 fi
 
