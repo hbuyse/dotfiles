5c527696d3bdbdecb6c3de192f73d20eda6ecada
henri.buyse <henri.buyse.external@stormshield.eu>
2020-11-30 09:13:37 +0100

    Revert "rofi: add uptime as title in powermenu"
    This reverts commit 217c2630d42656c4eabbde781a12ab164df08192.

--- .config/rofi/powermenu.sh
+++ .config/rofi/powermenu.sh
@@ -3,6 +3,4 @@
 exec 1> >(logger -s -p user.info -t "$(basename "$0")")
 exec 2> >(logger -s -p user.err -t "$(basename "$0")")
 
-UPTIME="$(uptime -p)"
-
-rofi -lines 7 -width 20 -show "${UPTIME}" -modi "${UPTIME}:${HOME}/.config/rofi/power.sh"
+rofi -lines 7 -width 20 -show Power -modi Power:"$HOME/.config/rofi/power.sh"
