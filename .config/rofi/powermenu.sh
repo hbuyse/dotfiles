c239c9049d823335701c5b93db7b565ce7b10f4a
Henri Buyse <henri.buyse@gmail.com>
2020-12-02 21:57:12 +0100

    rofi: powermenu show uptime

--- .config/rofi/powermenu.sh
+++ .config/rofi/powermenu.sh
@@ -3,18 +3,6 @@
 exec 1> >(logger -s -p user.info -t "$(basename "$0")")
 exec 2> >(logger -s -p user.err -t "$(basename "$0")")
 
-UPTIME="Power"
-
-vergte() {
-    [ "$1" = "$(echo -e "$1\n$2" | sort -rV | head -n1)" ]
-}
-
-vergt() {
-    [ "$1" = "$2" ] && return 1 || vergte $1 $2
-}
-
-if vergte "$(rofi -V | awk '{ print $2 }')" "1.6.0"; then
-    UPTIME="$(uptime -p)"
-fi
+UPTIME="$(uptime -p | sed -e 's/,//g')"
 
 rofi -lines 7 -width 20 -show "${UPTIME}" -modi "${UPTIME}:${HOME}/.config/rofi/power.sh"
