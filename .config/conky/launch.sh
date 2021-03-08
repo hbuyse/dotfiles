d6674ba138cef24dd246db4bd4f0d5371217bdcc
henri.buyse <henri.buyse.external@stormshield.eu>
2021-03-08 08:16:11 +0100

    conky: rework the launcher

--- .config/conky/launch.sh
+++ .config/conky/launch.sh
@@ -1,18 +1,23 @@
 #! /usr/bin/env bash
 
 DAEMON="conky"
-CONKY_CONFIG_FOLDER="$HOME/.config/conky"
-CONKIES=("$CONKY_CONFIG_FOLDER/conkyrc.date.time.$(hostname | tr '[:upper:]' '[:lower:]')" "$CONKY_CONFIG_FOLDER/conkyrc.proc.mem")
+CONKY_CONFIG_FOLDER="${HOME}/.config/conky"
+CONKIES=("${CONKY_CONFIG_FOLDER}/conkyrc.date.time.$(hostname | tr '[:upper:]' '[:lower:]')" "${CONKY_CONFIG_FOLDER}/conkyrc.proc.mem")
+LOG_FOLDER="/tmp/conky/"
 
 # Kill all processes even if the daemon binary does not exist
 # The daemon may have been uninstalled
-killall $DAEMON
+killall ${DAEMON}
 
 # Check that the daemon command exist
-if ! command -v $DAEMON 2> /dev/null; then
+if ! command -v ${DAEMON} 2> "/dev/null"; then
 	echo "Conky not found. Exiting"
 fi
 
+# Create log folder
+mkdir ${LOG_FOLDER}
+
+# Launch conkies
 for conky_conf in ${CONKIES[*]}; do
-	$DAEMON -c "$conky_conf" -d
+	${DAEMON} -c "${conky_conf}" -d > "$(mktemp -p ${LOG_FOLDER} conky.XXXXXXXXXX.log)" 2>&1
 done
