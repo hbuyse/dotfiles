3e26772748722fb1e30feb765e57611ae28e3768
Henri Buyse <henri.buyse@gmail.com>
2021-03-08 08:44:29 +0100

    xrandr: update screenlayout script

--- .screenlayout/layout.sh
+++ .screenlayout/layout.sh
@@ -1,18 +1,38 @@
 #!/usr/bin/env bash
+
+DAEMON="xrandr"
 HOSTNAME="$(hostname -s | tr '[:upper:]' '[:lower:]')"
-MONITORS_NB=$(xrandr --listmonitors | grep Monitors | awk '{ print $2 }')
+MONITORS_NB=$(xrandr --query | grep -wc connected)
 
-# If one monitor, do nothing and exit
-if [[ $MONITORS_NB -eq 1 ]]; then
-    exit 0
+# sleep 1
+OPTIONS=()
+OPTIONS+=(--output DP-1 --off)
+OPTIONS+=(--output HDMI-2 --off)
+
+if ! command -v $DAEMON > /dev/null 2>&1; then
+    echo "xrandr not found. Exiting." 1>&2
+    exit 1
+fi
+MONITORS_NB=3
+case $MONITORS_NB in
+    3)
+        OPTIONS+=(--output eDP-1 --primary --mode 1920x1080 --pos 0x1445 --rotate normal)
+        OPTIONS+=(--output HDMI-1 --off)
+        OPTIONS+=(--output DP-1-1 --mode 1920x1080 --pos 0x365 --rotate normal)
+        OPTIONS+=(--output DP-1-2 --mode 1920x1080 --pos 1920x0 --rotate left)
+        OPTIONS+=(--output DP-1-3 --off)
+        ;;
+    2)
+        OPTIONS+=(--output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal)
+        OPTIONS+=(--output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal)
+        ;;
+    *)
+        exit 0
+esac
+
+if [ "$1" = "-d" ]; then
+    OPTIONS+=(--verbose)
 fi
 
-if [ "$HOSTNAME" != "cg8250" ] && [ "$HOSTNAME" != "t480" ]; then
-
-    if [[ $MONITORS_NB -eq 3 ]]; then
-        xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x1445 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off --output DP-1-1 --mode 1920x1080 --pos 0x365 --rotate normal --output DP-1-2 --mode 1920x1080 --pos 1920x0 --rotate left --output DP-1-3 --off
-    elif [[ $MONITORS_NB -eq 2 ]]; then
-        xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-2 --off
-    fi
-fi
-# vim: set ts=4 sw=4 tw=0 et :
+eval $DAEMON ${OPTIONS[*]}
+ # vim: set ts=4 sw=4 tw=0 et ft=sh :
