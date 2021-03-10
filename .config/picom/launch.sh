3146ae4204f35514fbb8abeb666be93781997e22
Henri Buyse <henri.buyse@gmail.com>
2021-03-11 00:34:35 +0100

    picom: update launcher and chromium wm_name

--- .config/picom/launch.sh
+++ .config/picom/launch.sh
@@ -1,10 +1,12 @@
 #! /usr/bin/env bash
 
-DAEMON="picom"
+readonly DAEMON="picom"
 
 if [ ! command -v $DAEMON > /dev/null 2>&1 ]; then
 	echo "picom: no command found. Exiting"
 	exit 1
 fi
+
+pkill -9 $DAEMON
 
 $DAEMON
