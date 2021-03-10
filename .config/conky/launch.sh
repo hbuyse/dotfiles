b5f6633c6c5d6df3de619c6ad9407a381a537025
Henri Buyse <henri.buyse@gmail.com>
2021-03-11 00:27:33 +0100

    conky: rework launcher

--- .config/conky/launch.sh
+++ .config/conky/launch.sh
@@ -1,18 +1,18 @@
 #! /usr/bin/env bash
 
-DAEMON="conky"
-CONKY_CONFIG_FOLDER="${HOME}/.config/conky"
-CONKIES=("${CONKY_CONFIG_FOLDER}/conkyrc.date.time.$(hostname | tr '[:upper:]' '[:lower:]')" "${CONKY_CONFIG_FOLDER}/conkyrc.proc.mem")
-LOG_FOLDER="/tmp/conky/"
-
-# Kill all processes even if the daemon binary does not exist
-# The daemon may have been uninstalled
-killall ${DAEMON}
+readonly DAEMON="conky"
+readonly CONKY_CONFIG_FOLDER="${HOME}/.config/conky"
+readonly CONKIES=("${CONKY_CONFIG_FOLDER}/conkyrc.date.time.$(hostname | tr '[:upper:]' '[:lower:]')" "${CONKY_CONFIG_FOLDER}/conkyrc.proc.mem")
+readonly LOG_FOLDER="/tmp/conky/"
 
 # Check that the daemon command exist
 if ! command -v ${DAEMON} 2> "/dev/null"; then
 	echo "Conky not found. Exiting"
 fi
+
+# Kill all processes even if the daemon binary does not exist
+# The daemon may have been uninstalled
+killall -9 ${DAEMON}
 
 # Create log folder
 mkdir ${LOG_FOLDER}
