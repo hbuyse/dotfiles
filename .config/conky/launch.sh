30f6c3e931d39ac63626cdc81c3071a30188cf54
Henri Buyse <henri.buyse@gmail.com>
2021-03-13 19:18:40 +0100

    conky: launcher uses templates

--- .config/conky/launch.sh
+++ .config/conky/launch.sh
@@ -2,8 +2,10 @@
 
 readonly DAEMON="conky"
 readonly CONKY_CONFIG_FOLDER="${HOME}/.config/conky"
-readonly CONKIES=("${CONKY_CONFIG_FOLDER}/conkyrc.date.time.$(hostname | tr '[:upper:]' '[:lower:]')" "${CONKY_CONFIG_FOLDER}/conkyrc.proc.mem")
-readonly LOG_FOLDER="/tmp/conky/"
+readonly TMP_FOLDER="/tmp/conky/"
+readonly DATE_TIME_CONFIG="${TMP_FOLDER}/conkyrc.date.time"
+readonly PROC_CONFIG="${TMP_FOLDER}/conkyrc.proc.mem"
+readonly CONKIES=("${DATE_TIME_CONFIG}" "${PROC_CONFIG}")
 
 # Check that the daemon command exist
 if ! command -v ${DAEMON} > "/dev/null"; then
@@ -16,10 +18,22 @@ fi
 killall -q -9 ${DAEMON}
 
 # Create log folder
-mkdir -p ${LOG_FOLDER}
+mkdir -p ${TMP_FOLDER}
+
+# Create config date time
+cp "${CONKY_CONFIG_FOLDER}/conkyrc.date.time.before" "${DATE_TIME_CONFIG}"
+while read mountpoint
+do
+    echo "\$font\${color}$mountpoint:\${alignr}\${color2}\${fs_used $mountpoint} / \${fs_size $mountpoint}" >> ${DATE_TIME_CONFIG}
+done < <(mount | grep -E ^/dev | awk '{print $3}' | sort)
+echo ']]' >> "${DATE_TIME_CONFIG}"
+
+# Create config processes mem usage
+SCREEN_WIDTH=$(xrandr | grep " connected primary " | awk -F '[ x+]' '{print $5}')
+sed -e "s/CONKY_GAP_Y/$((SCREEN_WIDTH-230))/g" "${CONKY_CONFIG_FOLDER}/conkyrc.proc.mem.in" > ${PROC_CONFIG}
 
 # Launch conkies
 for conky_conf in ${CONKIES[*]}; do
-	${DAEMON} -c "${conky_conf}" -d > "$(mktemp -p ${LOG_FOLDER} conky.XXXXXXXXXX.log)" 2>&1
+	${DAEMON} -c "${conky_conf}" -d > "$(mktemp -p ${TMP_FOLDER} conky.XXXXXXXXXX.log)" 2>&1
 done
 # vim: set ts=4 sw=4 tw=0 et ft=sh :
