3d3e0399d4f90e89a95360b76829b3bf26f78d26
Henri Buyse <henri.buyse@gmail.com>
2021-03-16 12:05:57 +0100

    polybar: split

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,6 +4,7 @@ POLYBAR="polybar"
 XRANDR="xrandr"
 BAR="$(hostname -s | tr '[:upper:]' '[:lower:]')"
 LOG_DIR="/tmp/polybar"
+POLYBAR_CONFIG="$(dirname "${BASH_SOURCE[0]}")/config.ini"
 
 # Check that the commands exists
 if ! command -v ${POLYBAR} > /dev/null 2>&1; then
@@ -20,8 +21,8 @@ mkdir -p ${LOG_DIR}
 # If all your bars have ipc enabled, you can also use polybar-msg cmd quit
 if command -v "${XRANDR}" > /dev/null 2>&1; then
     for m in $(${XRANDR} --query | grep -w "connected" | cut -d" " -f1); do
-        MONITOR="${m}" ${POLYBAR} --reload "${BAR}"        > "${LOG_DIR}/${m}.log"        2>&1 &
-        MONITOR="${m}" ${POLYBAR} --reload "${BAR}-bottom" > "${LOG_DIR}/${m}-bottom.log" 2>&1 &
+        MONITOR="${m}" ${POLYBAR} --reload --config=${POLYBAR_CONFIG} "${BAR}"        > "${LOG_DIR}/${m}.log"        2>&1 &
+        MONITOR="${m}" ${POLYBAR} --reload --config=${POLYBAR_CONFIG} "${BAR}-bottom" > "${LOG_DIR}/${m}-bottom.log" 2>&1 &
     done
 else
     ${POLYBAR} --reload "${BAR}" > ${LOG_DIR}/main.log 2>&1 &
