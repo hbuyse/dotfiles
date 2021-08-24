572446ba1211d12dcef4cc1cc36ed2e22d418fcd
Henri Buyse <henri.buyse@gmail.com>
2021-08-24 13:11:00 +0200

    lsp: add lua-language-server

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -79,6 +79,52 @@ if has_snippets then
   capabilities.textDocument.completion.completionItem.snippetSupport = true;
 end
 
+local function prepare_sumneko_lua_language_server()
+  local system_name
+  if vim.fn.has("mac") == 1 then
+    system_name = "macOS"
+  elseif vim.fn.has("unix") == 1 then
+    system_name = "Linux"
+  elseif vim.fn.has('win32') == 1 then
+    system_name = "Windows"
+  else
+    return {}
+  end
+
+  -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
+  local sumneko_root_path = '/opt/lua-language-server'
+  local sumneko_binary = sumneko_root_path .. "/bin/" .. system_name .. "/lua-language-server"
+  local runtime_path = vim.split(package.path, ';')
+  table.insert(runtime_path, "lua/?.lua")
+  table.insert(runtime_path, "lua/?/init.lua")
+
+  return {
+    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+          -- Setup your lua path
+          path = runtime_path,
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        }
+      }
+    }
+  }
+end
+
 -- Use a loop to conveniently both setup defined servers and map buffer local keybindings when the language server attaches
 local servers = {
   ccls = {
@@ -93,9 +139,15 @@ local servers = {
   },
   bashls = {
     on_attach = on_attach
-  }
+  },
+  sumneko_lua = prepare_sumneko_lua_language_server(),
 }
+
 for lsp, conf in pairs(servers) do
-  lspconfig[lsp].setup { lspconf }
+  if conf['cmd'] ~= nil then
+    lspconfig[lsp].setup(conf)
+  else
+    lspconfig[lsp].setup { conf }
+  end
 end
 -- vim: set ts=2 sw=2 tw=0 et ft=lua :
