ef6d7f46953740977d60f1b1d34d9a0dd7423f70
Henri Buyse <henri.buyse@gmail.com>
2021-10-22 14:05:38 +0200

    nvim-mappings: change ':' into '<cmd>'

--- .config/nvim/lua/config/mappings.lua
+++ .config/nvim/lua/config/mappings.lua
@@ -5,42 +5,42 @@ end
 
 local setup_mappings = function()
   -- Telescope
-  kmap('n', '<C-p>',      ':lua require"telescope.builtin".find_files()<CR>')
-  kmap('n', '<leader>fs', ':lua require"telescope.builtin".live_grep()<CR>')
-  kmap('n', '<leader>fw', ':lua require"telescope.builtin".grep_string({search = vim.fn.expand("<cword>")})<CR>')
-  kmap('n', '<leader>fh', ':lua require"telescope.builtin".help_tags()<CR>')
-  kmap('n', '<leader>fb', ':lua require"telescope.builtin".buffers()<CR>')
-  kmap('n', '<leader>fq', ':lua require"telescope.builtin".quickfix()<CR>')
-  kmap('n', '<leader>fg', ':lua require"telescope.builtin".git_files()<CR>')
-  kmap('n', '<leader>fd', ':lua require"config.dotfiles".search_dotfiles({})<CR>')
+  kmap('n', '<C-p>',      '<cmd>lua require"telescope.builtin".find_files()<CR>')
+  kmap('n', '<leader>fs', '<cmd>lua require"telescope.builtin".live_grep()<CR>')
+  kmap('n', '<leader>fw', '<cmd>lua require"telescope.builtin".grep_string({search = vim.fn.expand("<cword>")})<CR>')
+  kmap('n', '<leader>fh', '<cmd>lua require"telescope.builtin".help_tags()<CR>')
+  kmap('n', '<leader>fb', '<cmd>lua require"telescope.builtin".buffers()<CR>')
+  kmap('n', '<leader>fq', '<cmd>lua require"telescope.builtin".quickfix()<CR>')
+  kmap('n', '<leader>fg', '<cmd>lua require"telescope.builtin".git_files()<CR>')
+  kmap('n', '<leader>fd', '<cmd>lua require"config.dotfiles".search_dotfiles({})<CR>')
 
   -- nvim-tree
-  kmap('n', '<F2>', ':NvimTreeToggle<CR>')
+  kmap('n', '<F2>', '<cmd>NvimTreeToggle<CR>')
 
   -- Misc
-  kmap('', '<leader> ', ':noh<CR>')
+  kmap('', '<leader> ', '<cmd>noh<CR>')
 
   -- Clipboard
   kmap('n', '<leader>y', '"+y')
   kmap('n', '<leader>yy', '"+yy')
   kmap('v', '<leader>y', '"+y')
-  kmap('n', '<leader>Y', ':%y+<CR>')
+  kmap('n', '<leader>Y', '<cmd>%y+<CR>')
   kmap('n', '<leader>p', '"+p')
   kmap('n', '<leader>pp', '"+p')
   kmap('v', '<leader>p', '"+p')
-  kmap('n', '<leader>P', ':%p+<CR>')
+  kmap('n', '<leader>P', '<cmd>%p+<CR>')
 
   -- Kommentary
   vim.api.nvim_set_keymap("n", "<leader>c<space>", "<Plug>kommentary_line_default", {noremap = false, silent = false})
   vim.api.nvim_set_keymap("x", "<leader>c<space>", "<Plug>kommentary_visual_default<C-c>", {noremap = false, silent = false})
 
   -- moveline
-  kmap('n', '<a-j>', ":move .+1<cr>==")
-  kmap('i', '<a-j>', "<esc>:move .+1<cr>==gi")
-  kmap('v', '<a-j>', ":move '>+1<cr>gv=gv")
-  kmap('n', '<a-k>', ":move .-2<cr>==")
-  kmap('i', '<a-k>', "<esc>:move .-2<cr>==gi")
-  kmap('v', '<a-k>', ":move '<-2<cr>gv=gv")
+  kmap('n', '<a-j>', "<cmd>move .+1<cr>==")
+  kmap('i', '<a-j>', "<esc><cmd>move .+1<cr>==gi")
+  kmap('v', '<a-j>', "<cmd>move '>+1<cr>gv=gv")
+  kmap('n', '<a-k>', "<cmd>move .-2<cr>==")
+  kmap('i', '<a-k>', "<esc><cmd>move .-2<cr>==gi")
+  kmap('v', '<a-k>', "<cmd>move '<-2<cr>gv=gv")
 
   -- Mappings to toggle folds
   -- With the following in your vimrc, you can toggle folds open/closed by pressing F9.
@@ -54,6 +54,9 @@ local setup_mappings = function()
   kmap('n', 'Y', 'y$')       -- Copy to the end of the line
   kmap('n', 'n', 'nzzzv')    -- Keep the search centered
   kmap('n', 'N', 'Nzzzv')    -- Keep the search centered
+
+  -- From one of my colleagues
+  kmap('n', '*', '*N')       -- Do not go to the next word but highlight all iterations of the current word
 
   -- Undo break points
   local undocharacters = {',', '.', '?', '!', '[', ']', '(', ')', '/', '\'', '"', ' '}
