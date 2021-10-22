070d9d017337b568ecac5c253f2adbb3c4e1352e
Henri Buyse <henri.buyse@gmail.com>
2021-10-22 20:03:11 +0200

    nvim-cmp: change config following breaking change (https://github.com/hrsh7th/nvim-cmp/issues/231)

--- .config/nvim/lua/config/packer.lua
+++ .config/nvim/lua/config/packer.lua
@@ -77,41 +77,44 @@ return require('packer').startup({function()
   use({
     'hrsh7th/nvim-cmp',
     requires = {
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
       { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
       { 'saadparwaiz1/cmp_luasnip' },
       { 'L3MON4D3/LuaSnip' },
       { 'rafamadriz/friendly-snippets' },
     },
     config = function()
+
+      local has_words_before = function()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local feedkey = function(key, mode)
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+      end
+
       -- luasnip setup
       local luasnip = require('luasnip')
 
       -- nvim-cmp setup
       local cmp = require('cmp')
 
-      --
-      local t = function(str)
-        return vim.api.nvim_replace_termcodes(str, true, true, true)
-      end
-
-      local check_back_space = function()
-        local col = vim.fn.col('.') - 1
-        return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
-      end
-
       cmp.setup({
-        --[[ completion = {
+        completion = {
           -- Does not perform completion automatically. I can still use manual completion though.
-          autocomplete = nil
-        }, ]]
+          autocomplete = false
+        },
         snippet = {
           expand = function(args)
             luasnip.lsp_expand(args.body)
           end,
         },
         mapping = {
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
@@ -120,31 +123,34 @@ return require('packer').startup({function()
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
           }),
-          ['<Tab>'] = function(fallback)
-            if vim.fn.pumvisible() == 1 then
-              vim.fn.feedkeys(t('<C-n>'), 'n')
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
-              vim.fn.feedkeys(t('<Plug>luasnip-expand-or-jump'), '')
-            elseif check_back_space() then
-              vim.fn.feedkeys(t('<tab>'), 'n')
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
             else
               fallback()
             end
-          end,
-          ['<S-Tab>'] = function(fallback)
-            if vim.fn.pumvisible() == 1 then
-              vim.fn.feedkeys(t('<C-p>'), 'n')
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then
-              vim.fn.feedkeys(t('<Plug>luasnip-jump-prev'), '')
+              luasnip.jump(-1)
             else
               fallback()
             end
-          end,
+          end, { "i", "s" }),
         },
         sources = {
-          { name = 'buffer' },
-          { name = 'nvim_lsp' },
           { name = 'luasnip' },
+          { name = 'nvim_lsp' },
+          { name = 'nvim_lua' },
+          { name = 'buffer' },
+          { name = 'path' },
         },
       })
     end,
