607324c0b11308033b89f7ffca3f5dcefaa33a03
Henri Buyse <henri.buyse@gmail.com>
2021-10-27 10:27:58 +0200

    nvim-lsp: update diagnostics virtual_text icon and display source if many

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -85,7 +85,10 @@ local on_attach = function(client, bufnr)
   vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = border })
   vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border })
   vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = true,
+    virtual_text = {
+      prefix = '●',
+      source = 'if_many',
+    },
     signs = true,
     underline = true,
   })
