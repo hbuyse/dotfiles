0db45abd2bf2e464329bac067562b9eee3bd7bf2
Henri Buyse <henri.buyse@gmail.com>
2021-12-02 12:03:21 +0100

    polybar: send notification when starting

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,6 +5,15 @@ XRANDR="xrandr"
 BAR="$(hostname -s | tr '[:upper:]' '[:lower:]')"
 LOG_DIR="/tmp/polybar"
 POLYBAR_CONFIG="$(dirname "${BASH_SOURCE[0]}")/config.ini"
+POLYBAR_START=()
+
+function join_by {
+    local d=${1-};
+    local f=${2-};
+    if shift 2; then
+        printf %s "$f" "${@/#/$d}"
+    fi
+}
 
 # Check that the commands exists
 if ! command -v ${POLYBAR} > /dev/null 2>&1; then
@@ -22,9 +31,15 @@ mkdir -p ${LOG_DIR}
 if command -v "${XRANDR}" > /dev/null 2>&1; then
     for m in $(${XRANDR} --query | grep -w "connected" | cut -d" " -f1); do
         MONITOR="${m}" ${POLYBAR} --reload --config=${POLYBAR_CONFIG} "${BAR}"        > "${LOG_DIR}/${m}.log"        2>&1 &
+        POLYBAR_START+=("${m}-${BAR}")
         MONITOR="${m}" ${POLYBAR} --reload --config=${POLYBAR_CONFIG} "${BAR}-bottom" > "${LOG_DIR}/${m}-bottom.log" 2>&1 &
+        POLYBAR_START+=("${m}-${BAR}-bottom")
     done
 else
     ${POLYBAR} --reload "${BAR}" > ${LOG_DIR}/main.log 2>&1 &
 fi
+
+
+notify-send "Polybar started" "$(join_by $'\n' ${POLYBAR_START[*]})"
+
 # vim: set ts=4 sw=4 tw=0 et :
