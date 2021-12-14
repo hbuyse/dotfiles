c2c4cba95f9bbd73902287491f0d71acfe097589
Henri Buyse <henri.buyse@gmail.com>
2021-12-15 00:45:39 +0100

    install.sh: install polybar venv, pip, oh-my-zsh, powerlevel10k

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
 DOTFILES_PATH="$(dirname "$(realpath "${0}")")"
+OH_MY_ZSH_DIR="$HOME/.oh-my-zsh"
 
 RED=$(printf '\033[31m')
 GREEN=$(printf '\033[32m')
@@ -65,8 +66,129 @@ finalize_git() {
 	fi
 }
 
+install_pip() {
+	local pip_url="https://bootstrap.pypa.io/get-pip.py"
+	echo -ne "- ${YELLOW}Installing pip...${RESET}"
+
+	if command -v pip > /dev/null; then
+		echo -e "${GREEN} already installed${RESET}"
+		return
+	fi
+
+	if ! command -v python > /dev/null; then
+		echo -e "\n${RED}To install pip, you need 'python'${RESET}" 1>&2
+		return
+	fi
+
+	# Download get-pip.py if python < 3.4
+	if [ "$(python --version | cut -d'.' -f1 | cut -d' ' -f2)" -lt 3 ] || [ "$(python --version | cut -d'.' -f2)" -lt 4 ]; then
+		# Get a filepath but do not create the file (-u is marked as 'unsafe' in the man)
+		local get_pip_filepath=$(mktemp -u)
+
+		if command -v curl > /dev/null; then
+			curl -qsSL "${get_pip_filepath}" -o "${get_pip_filepath}"
+		elif command -v wget > /dev/null; then
+			wget -q --no-check-certificate "${get_pip_filepath}" -O "${get_pip_filepath}"
+		else
+			echo -e "\n${RED}To install pip, you need either 'curl' or 'wget'${RESET}" 1>&2
+			return
+		fi
+
+		if [ ! -f "${get_pip_filepath}" ]; then
+			echo -e "\n${RED}Error while downloading 'get-pip.py'${RESET}" 1>&2
+			return
+		fi
+
+		python "${get_pip_filepath}" --quiet
+
+		# Remove the temporary file
+		rm -f "${get_pip_filepath}"
+	else
+		if ! python -m ensurepip --upgrade --user > /dev/null; then
+			echo -e "\n${RED}Error while running 'python -m ensurepip --user --upgrade'${RESET}" 1>&2
+		fi
+	fi
+
+	echo -e "${GREEN} done${RESET}"
+}
+
+polybar_venv() {
+	echo -ne "- ${YELLOW}Installing virtualenv for polybar...${RESET}"
+
+	local script_path="$HOME/.config/polybar/scripts"
+
+    if ! command -v python3 > /dev/null; then
+		echo -e "${RED}To install virtualenv for polybar scripts, you need to install 'python3'${RESET}" 1>&2
+		return
+	fi
+
+    if ! command -v virtualenv > /dev/null; then
+		echo -e "${RED}To install virtualenv for polybar scripts, you need to install 'virtualenv'${RESET}" 1>&2
+		echo -e "${RED}    pip install --user virtualenv${RESET}" 1>&2
+		return
+	fi
+
+	if [ ! -f "${script_path}/.venv/bin/python3" ]; then
+		virtualenv -p $(which python3) "${script_path}/.venv"
+		return
+	fi
+
+	"${script_path}/.venv/bin/pip" install -q -r "${script_path}/requirements.txt"
+
+	echo -e "${GREEN} done${RESET}"
+}
+
+install_oh_my_zsh() {
+	echo -ne "- ${YELLOW}Installing oh-my-zsh...${RESET}"
+	local
+
+	if [ -d "${OH_MY_ZSH_DIR}" ]; then
+		echo -e "${GREEN} already installed${RESET}"
+		return
+	fi
+
+	if command -v curl > /dev/null; then
+		ZSH="${OH_MY_ZSH_DIR}" sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	elif command -v wget > /dev/null; then
+		ZSH="${OH_MY_ZSH_DIR}" sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+	else
+		echo -e "\n${RED}To install oh-my-zsh, you need either 'curl' or 'wget'${RESET}" 1>&2
+		return
+	fi
+
+	echo -e "${GREEN} done${RESET}"
+}
+
+install_powerlevel10k() {
+	local install_dir="${OH_MY_ZSH_DIR}/custom/themes/powerlevel10k"
+
+	echo -ne "- ${YELLOW}Installing powerlevel10k...${RESET}"
+
+	if [ ! -d "${OH_MY_ZSH_DIR}" ]; then
+		echo -e "\n${RED}oh-my-zsh is not installed. Install it before continuing${RESET}" 1>&2
+		return
+	fi
+
+	if [ -d "${install_dir}" ]; then
+		echo -e "${GREEN} already installed${RESET}"
+		return
+	fi
+
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${install_dir}
+
+	echo -e "${GREEN} done${RESET}"
+}
 
 init_stow "$@"
 init_wallpapers
-finalize_git
+
+install_pip
+
+polybar_venv
+
 install_nerd_font "Hack"
+install_oh_my_zsh
+install_powerlevel10k
+
+# Always at the end to see it
+finalize_git
