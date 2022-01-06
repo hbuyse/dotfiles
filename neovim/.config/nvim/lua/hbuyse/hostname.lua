5b0d91a7e951e4e879fecf4a758ff4cdde44cf35
Henri Buyse <henri.buyse@gmail.com>
2022-01-06 01:22:25 +0100

    nvim: better handling between neovim versions

--- neovim/.config/nvim/lua/hbuyse/hostname.lua
+++ neovim/.config/nvim/lua/hbuyse/hostname.lua
@@ -2,6 +2,11 @@ local M = {}
 
 function M.getHostname()
   local f = io.open('/etc/hostname')
+
+  if not f then
+	return nil
+  end
+
   local hostname = f:read('*a') or ''
   f:close()
   hostname = string.gsub(hostname, '\n$', '')
