fed0c789909cd301912fa8cdc48c89bbe213855a
Henri Buyse <henri.buyse@gmail.com>
2022-01-10 10:48:29 +0100

    xautolock: generate lockscreen at restart of i3

--- wms/.config/i3/start_xautolock.sh
+++ wms/.config/i3/start_xautolock.sh
@@ -4,7 +4,14 @@ DAEMON="xautolock"
 HOSTNAME="$(hostname -s | tr '[:upper:]' '[:lower:]')"
 
 OPTS=("-time 5")
-OPTS+=("-locker \"betterlockscreen -l\"")
+if command -v betterlockscreen > /dev/null; then
+    # Generate lockscreen
+    betterlockscreen -u "$HOME/.config/wallpapers/locker.png"
+    OPTS+=("-locker \"betterlockscreen -l\"")
+else
+    OPTS+=("-locker \"i3lock\"")
+fi
+
 OPTS+=("-notify 30 -notifier \"notify-send -u critical -t 29000 'i3lock' 'Will lock in 30 seconds'\"")
 
 
@@ -13,12 +20,11 @@ if ! command -v $DAEMON; then
     exit
 fi
 
-killall $DAEMON
-
 if [[ $HOSTNAME == "t480" ]] || [[ $HOSTNAME == "cg8250" ]]; then
     OPTS+=("-killtime 15 -killer \"systemctl suspend\"")
     OPTS+=("-detectsleep")
 fi
 
+killall $DAEMON
 eval "${DAEMON}" "${OPTS[*]}"
 # vim: set ts=4 sw=4 tw=0 et ft=sh :
