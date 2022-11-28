c0537e5538882e0de473b19c45e7501dfb637a48
Henri Buyse <henri.buyse@gmail.com>
2022-11-28 11:49:46 +0100

    polybar: use tee to get the logs

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -22,7 +22,7 @@ if ! command -v ${POLYBAR} > /dev/null 2>&1; then
 fi
 
 # Terminate already running bar instances
-killall -q -9 "${POLYBAR}"
+killall -q "${POLYBAR}"
 
 # Create log directory
 mkdir -p ${LOG_DIR}
@@ -30,15 +30,14 @@ mkdir -p ${LOG_DIR}
 # If all your bars have ipc enabled, you can also use polybar-msg cmd quit
 if command -v "${XRANDR}" > /dev/null 2>&1; then
     for m in $(${XRANDR} --query | grep -w "connected" | cut -d" " -f1); do
-        MONITOR="${m}" ${POLYBAR} --reload --config=${POLYBAR_CONFIG} "${BAR}"        > "${LOG_DIR}/${m}.log"        2>&1 &
+        MONITOR="${m}" ${POLYBAR} --reload --config=${POLYBAR_CONFIG} "${BAR}" 2>&1 | tee -a "${LOG_DIR}/${m}.log" & disown
         POLYBAR_START+=("${m}-${BAR}")
-        MONITOR="${m}" ${POLYBAR} --reload --config=${POLYBAR_CONFIG} "${BAR}-bottom" > "${LOG_DIR}/${m}-bottom.log" 2>&1 &
+        MONITOR="${m}" ${POLYBAR} --reload --config=${POLYBAR_CONFIG} "${BAR}-bottom" 2>&1 | tee -a "${LOG_DIR}/${m}.log" & disown
         POLYBAR_START+=("${m}-${BAR}-bottom")
     done
 else
-    ${POLYBAR} --reload "${BAR}" > ${LOG_DIR}/main.log 2>&1 &
+    ${POLYBAR} --reload "${BAR}" 2>&1 | tee -a ${LOG_DIR}/main.log & disown
 fi
-
 
 notify-send "Polybar started" "$(join_by $'\n' ${POLYBAR_START[*]})"
 
