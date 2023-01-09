9f1ed1113595a92de77ac07df4aa8fe032aad806
Henri Buyse <henri.buyse@gmail.com>
2023-01-09 09:42:14 +0100

    i3: remove comment and killmenu scrips

--- dot_config/rofi/executable_killmenu.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-exec 1> >(logger -s -p user.info -t "$(basename "$0")")
-exec 2> >(logger -s -p user.err -t "$(basename "$0")")
-
-rofi -fullscreen -show Kill -modi Kill:"$HOME/.config/rofi/kill.sh"
