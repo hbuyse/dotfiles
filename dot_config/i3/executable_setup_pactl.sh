aa3bece0946e7527d0461bb114c7eef3231958fc
Henri Buyse <henri.buyse@gmail.com>
2023-01-09 09:39:03 +0100

    pactl: älways unload module-loopback before loading it again

--- dot_config/i3/executable_setup_pactl.sh
+++ dot_config/i3/executable_setup_pactl.sh
@@ -16,6 +16,7 @@ if [ "$HOSTNAME" = "hbuyse-latitude5400" ] || [ "$HOSTNAME" = "t480" ]; then
     ${CTL} set-sink-port alsa_output.pci-0000_00_1f.3.analog-stereo analog-output-headphones
 
     # Add loopback
+    ${CTL} unload-module module-loopback
     ${CTL} load-module module-loopback latency_msec=1
 fi
 # vim: set ts=4 sw=4 tw=0 et :
