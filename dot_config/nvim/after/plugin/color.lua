0e302d842e281a8a0c72a59561a911ca3bc7c0d1
Henri Buyse <henri.buyse@gmail.com>
2023-02-13 14:32:24 +0100

    nvim: fix colors

--- dot_config/nvim/after/plugin/color.lua
+++ dot_config/nvim/after/plugin/color.lua
@@ -1,31 +1,15 @@
 local palette = require('gruvbox.palette')
 
-vim.g.gruvbox_contrast_dark = 'medium'
-vim.g.gruvbox_sign_column = 'bg1'
-vim.g.gruvbox_hls_highlight = 'orange'
-require('gruvbox').setup()
-vim.cmd('colorscheme gruvbox')
-
 -- Set the colors for LSP floating window
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = '#4F4945' })
-vim.api.nvim_set_hl(0, 'FloatBorder', { bg = '#4F4945', fg = palette.light1 })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = palette.colors.dark1, fg = palette.colors.light1 })
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = palette.colors.dark1, fg = palette.colors.light1 })
 
 -- Set colorcolumn colors
-vim.api.nvim_set_hl(0, 'ColorColumn', { bg = palette.dark1 })
+vim.api.nvim_set_hl(0, 'ColorColumn', { bg = palette.colors.dark1 })
 
 -- ----------------
 -- Indent Blankline
 -- ----------------
-for name, color in pairs({
-  IndentBlanklineChar = palette.dark4, -- Highlight of indent character
-  IndentBlanklineSpaceChar = palette.dark4, -- Highlight of space character
-  -- IndentBlanklineSpaceCharBlankline = palette.neutral_red, -- Highlight of space character on blank lines.
-  IndentBlanklineContextChar = palette.light2, -- Highlight of indent character when base of current context
-  -- IndentBlanklineContextSpaceChar = palette.neutral_red, -- Highlight of space characters one indent level of the current context
-  -- IndentBlanklineContextStart = palette.neutral_red, -- Highlight of the first line of the current context.
-}) do
-  vim.api.nvim_set_hl(0, name, { fg = color, nocombine = true })
-end
 require('indent_blankline').setup({
   use_treesitter = true,
   show_current_context = true,
@@ -33,4 +17,33 @@ require('indent_blankline').setup({
   space_char_blankline = ' ',
   char_list = { '|', '¦', '┆', '┊' },
 })
+
+local group = vim.api.nvim_create_augroup('IndentBlanklineHighlight', { clear = true })
+vim.api.nvim_create_autocmd('ColorScheme', {
+  callback = function()
+    local indentblankline = {
+      IndentBlanklineChar = palette.colors.dark4, -- Highlight of indent character
+      IndentBlanklineSpaceChar = palette.colors.dark4, -- Highlight of space character
+      IndentBlanklineSpaceCharBlankline = palette.colors.neutral_red, -- Highlight of space character on blank lines.
+      IndentBlanklineContextChar = palette.colors.light2, -- Highlight of indent character when base of current context
+      IndentBlanklineContextSpaceChar = palette.neutral_red, -- Highlight of space characters one indent level of the current context
+      IndentBlanklineContextStart = palette.neutral_red, -- Highlight of the first line of the current context.
+    }
+    for name, color in pairs(indentblankline) do
+      vim.api.nvim_set_hl(0, name, { foreground = color, nocombine = true })
+    end
+  end,
+  group = group,
+})
+
+vim.api.nvim_set_hl(0, 'IndentBlanklineContextChar', { foreground = palette.colors.neutral_red, nocombine = true })
+vim.api.nvim_set_hl(0, 'IndentBlanklineContextStart', { special = palette.colors.visual_grey, underline = true })
+vim.api.nvim_set_hl(0, 'IndentBlanklineContextSpaceChar', { nocombine = true })
+
+vim.g.gruvbox_contrast_dark = 'medium'
+vim.g.gruvbox_sign_column = 'bg1'
+vim.g.gruvbox_hls_highlight = 'orange'
+vim.o.background = 'dark'
+require('gruvbox').setup()
+vim.cmd('colorscheme gruvbox')
 --  vim: set ts=2 sw=2 tw=0 noet ft=lua :
