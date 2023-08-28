878b327105fb82d2fef14ae13fedeb665b0a75c6
Henri Buyse <henri.buyse@gmail.com>
2023-08-28 16:22:40 +0200

    feat(nvim): change plugin manager from packer to lazy.nvim

---
+++ dot_config/nvim/lua/hbuyse/lazy_config.lua
@@ -0,0 +1,23 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup("hbuyse.plugins", {
+  defaults = {
+    lazy = false,
+    -- version = '*', -- try installing the latest stable versions of plugins
+  },
+})
+
+-- vim: set ts=2 sw=2 tw=0 et ft=lua :
---
+++ dot_config/nvim/lua/hbuyse/plugins/completion.lua
@@ -0,0 +1,18 @@
+return {
+  'hrsh7th/nvim-cmp',
+  -- event = 'InsertEvent',
+  dependencies = {
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-nvim-lua' },
+    { 'saadparwaiz1/cmp_luasnip' },
+
+    -- Snippets
+    { 'L3MON4D3/LuaSnip' },
+    { 'rafamadriz/friendly-snippets' },
+
+    -- Icons
+    { 'onsails/lspkind-nvim' },
+  },
+}
