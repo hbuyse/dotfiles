05b13635848384cd2eab9e515bc3ff9e41c00cbf
Henri Buyse <henri.buyse@gmail.com>
2023-09-01 16:39:26 +0200

    feat(nvim): install neogit

--- dot_config/nvim/lua/hbuyse/plugins/git.lua
+++ dot_config/nvim/lua/hbuyse/plugins/git.lua
@@ -1,7 +1,33 @@
 return {
   {
-    -- Git wrapper
-    'tpope/vim-fugitive',
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'nvim-telescope/telescope.nvim', -- optional
+      'sindrets/diffview.nvim', -- optional
+      'ibhagwan/fzf-lua', -- optional
+    },
+    config = true,
+    keys = {
+      {
+        '<leader>gs',
+        function()
+          require('neogit').open()
+        end,
+      },
+      {
+        '<leader>gc',
+        function()
+          require('neogit').open({ 'commit' })
+        end,
+      },
+      {
+        '<leader>gl',
+        function()
+          require('neogit').open({ 'log' })
+        end,
+      },
+    },
   },
   {
     -- Super fast git decorations
