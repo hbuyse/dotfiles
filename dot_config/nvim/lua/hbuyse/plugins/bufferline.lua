aaaebaf2ff4b594970144f4097fda0369f371ef1
Henri Buyse <henri.buyse@gmail.com>
2023-09-01 16:21:44 +0200

    fix(nvim): bufferline configuration

--- dot_config/nvim/lua/hbuyse/plugins/bufferline.lua
+++ dot_config/nvim/lua/hbuyse/plugins/bufferline.lua
@@ -3,26 +3,46 @@ return {
     -- bufferline (tabline)
     'akinsho/bufferline.nvim',
     dependencies = 'nvim-tree/nvim-web-devicons',
-    opts = {
-      options = {
-        mode = 'buffers',
-        color_icons = true,
-        show_buffer_close_icons = false,
-        show_close_icons = false,
-        modified_icon = '[+]',
-        separator_style = 'slant',
-        always_show_bufferline = true,
-        diagnostics = 'nvim_lsp',
-      },
-    },
-    init = function()
+    config = function()
       local palette = require('gruvbox.palette')
-      vim.api.nvim_set_hl(0, 'BufferLineSeparatorSelected', { fg = palette.colors.dark1 })
-      vim.api.nvim_set_hl(0, 'BufferLineSeparator', { fg = palette.colors.dark1 })
+      require('bufferline').setup({
+        options = {
+          mode = 'buffers',
+          color_icons = true,
+          show_buffer_close_icons = false,
+          show_close_icons = false,
+          modified_icon = '[+]',
+          separator_style = 'slant',
+          always_show_bufferline = true,
+          diagnostics = 'nvim_lsp',
+          offsets = {
+            {
+              filetype = 'neo-tree',
+              text = 'File Explorer',
+              highlight = 'Directory',
+              text_align = 'left',
+              separator = true,
+            },
+          },
+        },
+        highlights = {
+          separator = {
+            fg = palette.colors.dark1,
+          },
+          separator_selected = {
+            fg = palette.colors.dark1,
+          },
+        },
+      })
     end,
-    keys = {
-      { 'H', '<cmd>BufferLineCyclePrev<CR>', 'Move to previous bufferline' },
-      { 'L', '<cmd>BufferLineCycleNext<CR>', 'Move to next bufferline' },
-    },
+    init = function()
+      local keymaps = {
+        H = { cmd = '<cmd>BufferLineCyclePrev<CR>', desc = 'Move to previous bufferline' },
+        L = { cmd = '<cmd>BufferLineCycleNext<CR>', desc = 'Move to next bufferline' },
+      }
+      for k, v in pairs(keymaps) do
+        vim.keymap.set('n', k, v.cmd, { remap = false, silent = true, desc = v.desc })
+      end
+    end,
   },
 }
