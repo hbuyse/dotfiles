f51fb3c40bf334a358162cbd47cf5b45d1e20322
Henri Buyse <henri.buyse@gmail.com>
2023-10-09 17:45:30 +0200

    feat(nvim): update gruvbox config after tag 2.0.0

--- dot_config/nvim/lua/hbuyse/plugins/theme.lua
+++ dot_config/nvim/lua/hbuyse/plugins/theme.lua
@@ -3,18 +3,18 @@ return {
     'ellisonleao/gruvbox.nvim',
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      local palette = require('gruvbox.palette')
+      local palette = require('gruvbox').palette
 
       local group = vim.api.nvim_create_augroup('IndentBlanklineHighlight', { clear = false })
       vim.api.nvim_create_autocmd('ColorScheme', {
         callback = function()
           local indentblankline = {
-            IndentBlanklineChar = palette.colors.dark4, -- Highlight of indent character
-            IndentBlanklineSpaceChar = palette.colors.dark4, -- Highlight of space character
-            IndentBlanklineSpaceCharBlankline = palette.colors.neutral_red, -- Highlight of space character on blank lines.
-            IndentBlanklineContextChar = palette.colors.light2, -- Highlight of indent character when base of current context
-            IndentBlanklineContextSpaceChar = palette.colors.visual_grey, -- Highlight of space characters one indent level of the current context
-            IndentBlanklineContextStart = palette.colors.neutral_red, -- Highlight of the first line of the current context.
+            IndentBlanklineChar = palette.dark4, -- Highlight of indent character
+            IndentBlanklineSpaceChar = palette.dark4, -- Highlight of space character
+            IndentBlanklineSpaceCharBlankline = palette.neutral_red, -- Highlight of space character on blank lines.
+            IndentBlanklineContextChar = palette.light2, -- Highlight of indent character when base of current context
+            IndentBlanklineContextSpaceChar = palette.visual_grey, -- Highlight of space characters one indent level of the current context
+            IndentBlanklineContextStart = palette.neutral_red, -- Highlight of the first line of the current context.
           }
           for name, color in pairs(indentblankline) do
             vim.api.nvim_set_hl(0, name, { foreground = color, bold = true, nocombine = true })
@@ -30,8 +30,8 @@ return {
       require('gruvbox').setup({
         overrides = {
           -- Set the colors for LSP floating window
-          NormalFloat = { bg = palette.colors.dark1, fg = palette.colors.light1 },
-          FloatBorder = { bg = palette.colors.dark1, fg = palette.colors.light1 },
+          NormalFloat = { bg = palette.dark1, fg = palette.light1 },
+          FloatBorder = { bg = palette.dark1, fg = palette.light1 },
         },
       })
       vim.cmd('colorscheme gruvbox')
@@ -88,7 +88,7 @@ return {
     'akinsho/bufferline.nvim',
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
-      local palette = require('gruvbox.palette')
+      local palette = require('gruvbox').palette
       require('bufferline').setup({
         options = {
           mode = 'buffers',
@@ -111,10 +111,10 @@ return {
         },
         highlights = {
           separator = {
-            fg = palette.colors.dark1,
+            fg = palette.dark1,
           },
           separator_selected = {
-            fg = palette.colors.dark1,
+            fg = palette.dark1,
           },
         },
       })
