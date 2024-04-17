fe4945ba1838a88405654603f429636d535c3809
Henri Buyse <henri.buyse@gmail.com>
2024-04-17 12:50:46 +0200

    fix(nvim): do not display indentation on some specific file types

--- dot_config/nvim/lua/hbuyse/plugins/indent.lua
+++ dot_config/nvim/lua/hbuyse/plugins/indent.lua
@@ -24,6 +24,31 @@ return {
         indent = {
           char = { '|', '¦', '┆', '┊' },
         },
+        exclude = {
+          filetypes = {
+            'startify',
+            'dashboard',
+            'dotooagenda',
+            'log',
+            'fugitive',
+            'gitcommit',
+            'packer',
+            'vimwiki',
+            'markdown',
+            'json',
+            'txt',
+            'vista',
+            'help',
+            'todoist',
+            'NvimTree',
+            'peekaboo',
+            'git',
+            'TelescopePrompt',
+            'undotree',
+            'flutterToolsOutline',
+            '', -- for all buffers without a file type
+          },
+        },
       })
       hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
     end,
