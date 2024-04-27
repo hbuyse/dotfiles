a7e00fb370d376ccd90aa27bf3939e70e7fe0362
Henri Buyse <henri.buyse@gmail.com>
2024-04-27 15:35:21 +0200

    fix(scripts): move cargo_install, cargo_git_install and pip_install to utils.sh

--- run_onchange_after_install-pip-packages.sh
+++ run_onchange_after_install-pip-packages.sh
@@ -7,30 +7,10 @@
 PIP_PATH="${HOME}/.pyenv/shims/pip"
 PYENV_PATH="${HOME}/.pyenv/bin/pyenv"
 
-pip_install() {
-    local package="${1}"
-    local version="${2}"
-
-    if "${PIP_PATH}" freeze --local | grep -q "${package}==${version}"; then
-        prompt "Installing '${package} v${version}' using pip"
-        display_already_installed
-    else
-        local upgrade_opt=""
-        if "${PIP_PATH}" freeze --local | grep -q "${package}"; then
-            prompt "Updating '${package}' to version '${version}' using pip"
-            upgrade_opt="--upgrade"
-        else
-            prompt "Installing '${package} v${version}' using pip"
-        fi
-        "${PIP_PATH}" install --quiet --user ${upgrade_opt} "${package}==${version}"
-        display_ko_ok ${?}
-    fi
-}
-
 display_info "${0}"
 
 PYTHON_VERSION="3.10"
-if ! command -v python3 | grep "${HOME}" > /dev/null; then
+if ! command -v python3 | grep "${HOME}" >/dev/null; then
     prompt "Install Python ${PYTHON_VERSION}"
     "${PYENV_PATH}" install -f "${PYTHON_VERSION}"
     display_ko_ok ${?}
