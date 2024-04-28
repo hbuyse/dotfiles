4e1bc5c575f0fdade927690750c679a3534f2d1c
Henri Buyse <henri.buyse@gmail.com>
2024-04-28 17:42:16 +0200

    feat(scripts): split installation of rustup, cargo packages and lsp-servers

---
+++ run_onchange_after_install-cargo-packages.sh
@@ -0,0 +1,47 @@
+#! /usr/bin/env bash
+
+# Load the shell lib
+. "${CHEZMOI_WORKING_TREE}/utils.sh"
+
+display_info "${0}"
+
+# Do not install if truenas
+if [[ "${HOSTNAME}" == "truenas" ]]; then
+    echo "Truenas host. Passing to next script"
+    exit 0
+fi
+
+case "${OS}-${ID}" in
+"linux-manjaro" | "linux-arch")
+    install_packages ripgrep bottom fd stylua dust alacritty texlab
+    exit 0
+    ;;
+
+"linux-ubuntu" | "freebsd-freebsd")
+    # shellcheck source=/dev/null
+    source "${HOME}/.cargo/env"
+
+    # Install cargo package
+    if cmdexists cargo; then
+        declare -A CARGO_GIT_PKGS=()
+        declare -A CARGO_PKGS=(
+            ["bottom"]="0.9.6"
+            ["fd-find"]="9.0.0"
+            ["stylua"]="0.20.0"
+            ["ripgrep"]="14.1.0"
+            ["du-dust"]="0.9.0"
+        )
+
+        if [[ "${OS}" == "linux" ]]; then
+            CARGO_PKGS+=(
+                ["alacritty"]="0.13.1"
+            )
+        fi
+
+        for pkg in "${!CARGO_GIT_PKGS[@]}"; do
+            cargo_git_install "${pkg}" "${CARGO_GIT_PKGS[${pkg}]}"
+        done
+    fi
+    ;;
+*) ;;
+esac
---
+++ run_onchange_install-rustup.sh
@@ -0,0 +1,38 @@
+#! /usr/bin/env bash
+
+# Load the shell lib
+. "${CHEZMOI_WORKING_TREE}/utils.sh"
+
+display_info "${0}"
+
+# Do not install if truenas
+if [[ "${HOSTNAME}" == "truenas" ]]; then
+    echo "Truenas host. Passing to next script"
+    exit 0
+fi
+
+case "${OS}-${ID}" in
+"linux-manjaro" | "linux-arch")
+    install_packages rustup
+    exit 0
+    ;;
+
+"linux-ubuntu" | "freebsd-freebsd")
+    [[ "${OS}" == "freebsd" ]] && RUST_DEFAULT_HOST="x86_64-unknown-freebsd" || RUST_DEFAULT_HOST="x86_64-unknown-linux-gnu"
+    if ! cmdexists rustup; then
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -q --no-modify-path --default-host "${RUST_DEFAULT_HOST}" --default-toolchain none --profile default -y
+        # shellcheck source=/dev/null
+        source "${HOME}/.cargo/env"
+        rustup default stable
+    # Use the redirection since rustup check throw a Broken Pipe when filtering with grep -q
+    elif ! rustup check | grep "stable-${RUST_DEFAULT_HOST} - Up to date" >/dev/null; then
+        rustup update
+    fi
+
+    # Rust compiler
+    if ! cmdexists "rustc"; then
+        rustup install stable
+    fi
+    ;;
+*) ;;
+esac
