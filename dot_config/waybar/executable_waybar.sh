4e4c4bb81c233d2ffb8e2b244dc4cad5e182b2ba
Henri Buyse <henri.buyse@gmail.com>
2024-05-01 17:29:27 +0200

    feat(sway): waybar starts using exec_always command (not bar command)

--- dot_config/waybar/executable_waybar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# wrapper script for waybar with args, see https://github.com/swaywm/sway/issues/5724
-
-USER_CONFIG=$HOME/.config/waybar/config.jsonc
-USER_STYLE=$HOME/.config/waybar/style.css
-
-killall -9 waybar
-waybar -c "${USER_CONFIG}" -s "${USER_STYLE}" &
