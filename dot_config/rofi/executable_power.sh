bd5ef96bfb19f5b8b290b379b76dd0a89bd7a80a
Henri Buyse <henri.buyse@gmail.com>
2024-05-01 18:47:10 +0200

    feat(rofi): parse multiple folders to find the correct one in power

--- dot_config/rofi/executable_power.sh
+++ dot_config/rofi/executable_power.sh
@@ -8,30 +8,38 @@ lock() {
 list() {
     local size
     local icon_name
+    local icon_folder
     icon_name="$1"
     size="$2"
 
-    echo -en "Lock\0icon\x1f${HOME}/.local/share/icons/${icon_name}/${size}x${size}/apps/system-lock-screen.svg\n"
-    echo -en "Logout\0icon\x1f${HOME}/.local/share/icons/${icon_name}/${size}x${size}/apps/system-log-out.svg\n"
-    echo -en "Shutdown\0icon\x1f${HOME}/.local/share/icons/${icon_name}/${size}x${size}/apps/system-shutdown.svg\n"
-    echo -en "Reboot\0icon\x1f${HOME}/.local/share/icons/${icon_name}/${size}x${size}/apps/system-reboot.svg\n"
-    echo -en "Hibernate\0icon\x1f${HOME}/.local/share/icons/${icon_name}/${size}x${size}/apps/system-suspend-hibernate.svg\n"
-    echo -en "Suspend\0icon\x1f${HOME}/.local/share/icons/${icon_name}/${size}x${size}/apps/system-suspend.svg\n"
+    for folder in "${HOME}/.local/share/icons" "${HOME}/.icons" "/usr/local/share/icons" "/usr/share/icons"; do
+        if [ -d "${folder}/${icon_name}" ]; then
+            icon_folder="${folder}/${icon_name}"
+            break
+        fi
+    done
+
+    echo -en "Lock\0icon\x1f${icon_folder}/${size}x${size}/apps/system-lock-screen.svg\n"
+    echo -en "Logout\0icon\x1f${icon_folder}/${size}x${size}/apps/system-log-out.svg\n"
+    echo -en "Shutdown\0icon\x1f${icon_folder}/${size}x${size}/apps/system-shutdown.svg\n"
+    echo -en "Reboot\0icon\x1f${icon_folder}/${size}x${size}/apps/system-reboot.svg\n"
+    echo -en "Hibernate\0icon\x1f${icon_folder}/${size}x${size}/apps/system-suspend-hibernate.svg\n"
+    echo -en "Suspend\0icon\x1f${icon_folder}/${size}x${size}/apps/system-suspend.svg\n"
     if pgrep --exact redshift > /dev/null; then
-        echo -en "Deactivate Redshift\0icon\x1f${HOME}/.local/share/icons/${icon_name}/${size}x${size}/panel/redshift-status-off.svg\n"
+        echo -en "Deactivate Redshift\0icon\x1f${icon_folder}/${size}x${size}/panel/redshift-status-off.svg\n"
     else
-        echo -en "Activate Redshift\0icon\x1f${HOME}/.local/share/icons/${icon_name}/${size}x${size}/panel/redshift-status-on.svg\n"
+        echo -en "Activate Redshift\0icon\x1f${icon_folder}/${size}x${size}/panel/redshift-status-on.svg\n"
     fi
     if [[ "$(xset q | grep "DPMS is " | awk '{ print $3 }' | tr "[:upper:]" "[:lower:]")" == "disabled" ]]; then
-        echo -en "Uncaffeinate\0icon\x1f${HOME}/.local/share/icons/${icon_name}/${size}x${size}/panel/caffeine-cup-empty.svg\n"
+        echo -en "Uncaffeinate\0icon\x1f${icon_folder}/${size}x${size}/panel/caffeine-cup-empty.svg\n"
     else
-        echo -en "Caffeinate\0icon\x1f${HOME}/.local/share/icons/${icon_name}/${size}x${size}/panel/caffeine-cup-full.svg\n"
+        echo -en "Caffeinate\0icon\x1f${icon_folder}/${size}x${size}/panel/caffeine-cup-full.svg\n"
     fi
     if pgrep dunst > "/dev/null"; then
         if [[ "$(dunstctl is-paused)" == "true" ]]; then
-            echo -en "Do Disturb\0icon\x1f${HOME}/.local/share/icons/${icon_name}/${size}x${size}/apps/bell.svg\n"
+            echo -en "Do Disturb\0icon\x1f${icon_folder}/${size}x${size}/apps/bell.svg\n"
         elif [[ "$(dunstctl is-paused)" == "false" ]]; then
-            echo -en "Do Not Disturb\0icon\x1f${HOME}/.local/share/icons/${icon_name}/${size}x${size}/apps/bell.svg\n"
+            echo -en "Do Not Disturb\0icon\x1f${icon_folder}/${size}x${size}/apps/bell.svg\n"
         fi
     fi
 }
