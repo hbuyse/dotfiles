ddd0a88da2e8c7d534eabb140b83fb0206f20300
Henri Buyse <henri.buyse@gmail.com>
2024-05-27 19:50:33 +0200

    fix(scripts): remove lsp server installation from npm packages install script

--- .chezmoiscripts/run_onchange_after_install-npm-packages.sh
+++ .chezmoiscripts/run_onchange_after_install-npm-packages.sh
@@ -14,16 +14,11 @@ if cmdexists npm; then
     mkdir -p "${prefix}"
 
     declare -A NPM_PKGS=(
-        ["bash-language-server"]="5.1.2"
         ["diff-so-fancy"]="1.4.3"
         ["neovim"]="5.0.1"
         ["npm"]="10.5.0"
         ["prettier"]="3.2.5"
-        ["pyright"]="1.1.353"
         ["typescript"]="5.4.2"
-        ["typescript-language-server"]="4.3.3"
-        ["vscode-langservers-extracted"]="4.8.0"
-        ["yaml-language-server"]="1.14.0"
     )
 
     for pkg in "${!NPM_PKGS[@]}"; do
