0f0bda2fd4f2fa64348e7cd68432399a3b919656
Henri Buyse <henri.buyse@gmail.com>
2024-05-27 10:38:30 +0200

    feat(nvim): add Oil plugin (file explorer)

--- dot_config/nvim/lua/hbuyse/plugins/tree.lua
+++ dot_config/nvim/lua/hbuyse/plugins/tree.lua
@@ -1,5 +1,16 @@
 return {
   {
+    'stevearc/oil.nvim',
+    opts = {
+      default_file_explorer = true,
+    },
+    -- Optional dependencies
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    keys = {
+      { '-', '<cmd>Oil<cr>', desc = 'Open parent directory' },
+    },
+  },
+  {
     'nvim-neo-tree/neo-tree.nvim',
     branch = 'v3.x',
     dependencies = {
