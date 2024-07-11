84c0983e6415a6395443e6ee08ecb8271a42cc42
Henri Buyse <henri.buyse@gmail.com>
2024-07-11 23:49:54 +0200

    feat(wallpaper): generate coding image as svg and generate image using envsubst

--- dot_config/wallpapers/executable_generate-bg.sh
+++ dot_config/wallpapers/executable_generate-bg.sh
@@ -7,3 +7,5 @@ export BACKGROUND="$3"
 
 # shellcheck disable=SC2002
 cat "$HOME/.config/wallpapers/coding.svg" | envsubst > "$HOME/.config/wallpapers/generated_coding.svg"
+ln -sf "$HOME/.config/wallpapers/generated_coding.svg" "$HOME/.config/wallpapers/wallpaper"
+ln -sf "$HOME/.config/wallpapers/generated_coding.svg" "$HOME/.config/wallpapers/locker"
