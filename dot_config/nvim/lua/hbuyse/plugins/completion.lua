5a6116f4f2e5b36f7a7ebc71b13e4ae35b46cceb
Henri Buyse <henri.buyse@gmail.com>
2024-07-30 16:50:44 +0200

    feat(nvim): update cmp

--- dot_config/nvim/lua/hbuyse/plugins/completion.lua
+++ dot_config/nvim/lua/hbuyse/plugins/completion.lua
@@ -6,6 +6,7 @@ end
 return {
   {
     'hrsh7th/nvim-cmp',
+    -- dependencies
     -- load cmp on InsertEnter
     event = 'InsertEnter',
     -- these dependencies will only be loaded when cmp loads
@@ -15,32 +16,43 @@ return {
       { 'hrsh7th/cmp-path' },
       { 'hrsh7th/cmp-nvim-lsp' },
       { 'hrsh7th/cmp-nvim-lua' },
-      { 'saadparwaiz1/cmp_luasnip' },
 
       -- Snippets
-      { 'L3MON4D3/LuaSnip' },
-      { 'rafamadriz/friendly-snippets' },
+      {
+        'garymjr/nvim-snippets',
+        opts = { friendly_snippets = true },
+        dependencies = { 'rafamadriz/friendly-snippets' },
+      },
 
       -- Icons
       { 'onsails/lspkind-nvim' },
     },
-    config = function()
-      -- luasnip setup
-      local luasnip = require('luasnip')
-      require('luasnip.loaders.from_vscode').lazy_load()
+    opts = function()
+      vim.api.nvim_set_hl(0, 'CmpGhostText', { link = 'Comment', default = true })
 
-      -- nvim-cmp setup
       local cmp = require('cmp')
+      local defaults = require('cmp.config.default')()
+      local auto_select = true
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
+      return {
+        -- Disabling completion in certain contexts, such as comments
+        enabled = function()
+          -- disable completion in comments
+          local context = require('cmp.config.context')
+          -- keep command mode completion enabled when cursor is in a comment
+          if vim.api.nvim_get_mode().mode == 'c' then
+            return true
+          else
+            return not context.in_treesitter_capture('comment') and not context.in_syntax_group('Comment')
+          end
+        end,
+        -- configure any filetype to auto add brackets
+        auto_brackets = {},
+        completion = {
+          completeopt = 'menu,menuone,noinsert' .. (auto_select and '' or ',noselect'),
         },
+        preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
         mapping = {
-          ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
@@ -51,31 +63,29 @@ return {
           }),
           ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
+              -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
               cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
+            elseif vim.snippet.active({ direction = 1 }) then
+              vim.schedule(function()
+                vim.snippet.jump(1)
+              end)
             elseif has_words_before() then
               cmp.complete()
             else
               fallback()
             end
-          end, {
-            'i',
-            's',
-          }),
-
+          end, { 'i', 's' }),
           ['<S-Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
+            elseif vim.snippet.active({ direction = -1 }) then
+              vim.schedule(function()
+                vim.snippet.jump(-1)
+              end)
             else
               fallback()
             end
-          end, {
-            'i',
-            's',
-          }),
+          end, { 'i', 's' }),
         },
         sources = {
           { name = 'luasnip' },
@@ -100,9 +110,12 @@ return {
         },
         experimental = {
           native_menu = false,
-          ghost_text = true,
+          ghost_text = {
+            hl_group = 'CmpGhostText',
+          },
         },
-      })
+        sorting = defaults.sorting,
+      }
     end,
   },
 }
