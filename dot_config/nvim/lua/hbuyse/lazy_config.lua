ed09b58b3386241695f7640eaeb6e0b751fe4469
Henri Buyse <henri.buyse@gmail.com>
2024-08-07 23:18:17 +0200

    feat(nvim): lazy - limit updates to 4 jobs

--- dot_config/nvim/lua/hbuyse/lazy_config.lua
+++ dot_config/nvim/lua/hbuyse/lazy_config.lua
@@ -13,11 +13,13 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('hbuyse.plugins', {
+require('lazy').setup({
+  spec = 'hbuyse.plugins',
   defaults = {
     lazy = false,
     -- version = '*', -- try installing the latest stable versions of plugins
   },
+  concurrency = 4,
 })
 
 -- vim: set ts=2 sw=2 tw=0 et ft=lua :
